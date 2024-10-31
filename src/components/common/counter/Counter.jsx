aeebf815d867e04d0069fd85e283db25f019b33b
Tuki <ktomar16@gmail.com>
2024-10-30 21:56:39 -0300

    some styles added to ItemDetail

--- src/components/common/counter/Counter.jsx
+++ src/components/common/counter/Counter.jsx
@@ -1,10 +1,54 @@
 const Counter = ({ contador, sumar, restar, onAdd }) => {
   return (
-    <div>
-      <button onClick={restar}>-</button>
-      <h2>{contador}</h2>
-      <button onClick={sumar}>+</button>
+    <div style={{ display: "flex" }}>
       <button
+        style={{
+          backgroundColor: "#088178",
+          color: "#fff",
+          fontSize: "14px",
+          fontWeight: "700",
+          padding: "10px 20px",
+          borderRadius: "4px",
+          cursor: "pointer",
+          border: "none",
+          outline: "none",
+          transition: "0.2s ease",
+        }}
+        onClick={restar}
+      >
+        -
+      </button>
+      <h2 style={{ padding: "10px" }}> {contador} </h2>
+      <button
+        style={{
+          backgroundColor: "#088178",
+          color: "#fff",
+          fontSize: "14px",
+          fontWeight: "700",
+          padding: "10px 20px",
+          borderRadius: "4px",
+          cursor: "pointer",
+          border: "none",
+          outline: "none",
+          transition: "0.2s ease",
+        }}
+        onClick={sumar}
+      >
+        +
+      </button>
+      <button
+        style={{
+          backgroundColor: "#088178",
+          color: "#fff",
+          fontSize: "14px",
+          fontWeight: "700",
+          padding: "10px 20px",
+          borderRadius: "4px",
+          cursor: "pointer",
+          border: "none",
+          outline: "none",
+          transition: "0.2s ease",
+        }}
         onClick={() => {
           onAdd(contador);
         }}
